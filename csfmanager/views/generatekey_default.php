f1afe9f8640eaf212367ebb36dcd7a1959236267
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-02-19 13:02:56 -0300

    Fix the `Generate Email Keys` tab.
    For some reason this file had more errors than the database extension.
    Some variable was mising.

--- csfmanager/views/generatekey_default.php
+++ csfmanager/views/generatekey_default.php
@@ -9,6 +9,8 @@
  *
  **/
 
+use WHMCS\Database\Capsule;
+
 if (!defined("JETCSFMANAGER"))
 	die("This file cannot be accessed directly");
 
@@ -25,46 +27,52 @@ class jcsf_generatekey_default
 		$output['data']['generate'] = csfmanager::request_var('generate', array());
 		$output['data']['servers'] = array();
 		
-		$sql = "SELECT *
-			FROM tblservers
-			" . (trim($instance->getConfig('servers', '')) ? "WHERE id IN (" . trim($instance->getConfig('servers', '')) . ")" : '');
-		$result = mysql_query($sql);
-		
-		while($server_details = mysql_fetch_assoc($result))
-		{
-			$output['data']['servers'][$server_details['id']] = array_merge($server_details, array('password' => decrypt($server_details['password'], $cc_encryption_hash)));
-		}
-		mysql_free_result($result);
-		
+        $builder = Capsule::table('tblservers');
+
+        if (trim($instance->getConfig('servers', ''))) {
+            $builder->whereIn('id', trim($instance->getConfig('servers', '')));
+        }
+
+        $result = $builder->get();
+
+        $resultAsArray = json_decode(json_encode($result), true);
+
+        foreach ($resultAsArray as $server_details) {
+            $output['data']['servers'][$server_details['id']] = array_merge($server_details, array('password' => decrypt($server_details['password'], $cc_encryption_hash)));
+        }
+
 		$output['data']['clients'] = array();
-		
-		$sql = "SELECT c.*, h.domain, p.id as product_id, s.id as server_id, h.id as hosting_id
-			FROM tblclients as c
-			INNER JOIN tblhosting as h
-			ON h.userid = c.id
-			INNER JOIN tblproducts as p
-			ON p.id = h.packageid
-			INNER JOIN tblservers as s
-			ON s.id = h.server
-			WHERE c.status = 'Active'
-			AND h.domainstatus = 'Active'
-			" . (trim($instance->getConfig('servers', '')) ? "AND s.id IN (" . trim($instance->getConfig('servers', '')) . ")" : '') . "
-			AND p.type IN ('hostingaccount','reselleraccount','server')
-			ORDER BY c.firstname ASC, c.lastname ASC, c.id ASC";
-		$result = mysql_query($sql);
-		
-		while($client_details = mysql_fetch_assoc($result))
-		{                     
-			$output['data']['clients'][$client_details['id']] = $client_details;
-		}
-		mysql_free_result($result);
-		
+
+        $builder = Capsule::table('tblclients as c')
+            ->join('tblhosting as h', 'h.userid', '=', 'c.id')
+            ->join('tblproducts as p', 'p.id', '=', 'h.packageid')
+            ->join('tblservers as s', 's.id', '=', 'h.server')
+            ->where('c.status', '=', 'Active');
+
+        if (trim($instance->getConfig('servers', ''))) {
+            $builder->whereIn('s.id', trim($instance->getConfig('servers', '')));
+        }
+
+        $builder->whereIn('p.type', ['hostingaccount', 'reselleraccount', 'server'])
+            ->orderBy('c.firstname', 'ASC')
+            ->orderBy('c.lastname', 'ASC')
+            ->orderBy('c.id', 'ASC');
+
+        $result = $builder->get();
+
+        $resultAsArray = json_decode(json_encode($result), true);
+
+        foreach ($resultAsArray as $client_details) {
+            $output['data']['clients'][$client_details['id']] = $client_details;
+        }
+
 		return $output;
 	}
 
 	public function create()
 	{	
 		global $cc_encryption_hash, $instance, $CONFIG;
+        $uid = intval($_SESSION['uid']);
 		
 		$output = $this->_default();
 		if(!$output['success']) return $output;
@@ -96,11 +104,20 @@ class jcsf_generatekey_default
 	
 			if($sendmail['success'])
 			{
-				logActivity("Jetserver CSF Manager :: The admin sent allow ket to the recipient {$email} ({$fullname}) on behalf of <a href=\"clientssummary.php?userid={$uid}\">Client ID: {$uid}</a>");
+				logActivity("Jetserver CSF Manager :: The admin sent allow ket to the recipient {$output['data']['generate']['email']} ({$output['data']['generate']['recipient']}) on behalf of <a href=\"clientssummary.php?userid={$uid}\">Client ID: {$uid}</a>");
 	
-				$sql = "INSERT INTO mod_csfmanager_allow_keys (`user_id`,`server_id`,`product_id`,`key_hash`,`key_recipient`,`key_email`,`key_clicks_remained`,`key_expire`) VALUES
-					('{$client_id}','{$output['data']['clients'][$client_id]['server_id']}','{$output['data']['clients'][$client_id]['hosting_id']}','{$hashkey}','{$output['data']['generate']['recipient']}','{$output['data']['generate']['email']}',{$valid_clicks},'" . (time() + (60 * 60 * 24 * $valid_days)) . "')";
-				mysql_query($sql);
+                Capsule::table('mod_csfmanager_allow_keys')->insert(
+                    [
+                        'user_id' => $client_id,
+                        'server_id' => $output['data']['clients'][$client_id]['server_id'],
+                        'product_id' => $output['data']['clients'][$client_id]['hosting_id'],
+                        'key_hash' => $hashkey,
+                        'key_recipient' => $output['data']['generate']['recipient'],
+                        'key_email' => $output['data']['generate']['email'],
+                        'key_clicks_remained' => $valid_clicks,
+                        'key_expire' => (time() + (60 * 60 * 24 * $valid_days)),
+                    ]
+                );
 	
 				$output['success'] = true;
 				$output['message'] = $instance->lang('emailsent');
@@ -114,7 +131,7 @@ class jcsf_generatekey_default
 		{
 			if(!$output['data']['generate']['recipient']) $output['errormessages'][] = $instance->lang('emptyrecipientname');
 			if(!$output['data']['generate']['email']) $output['errormessages'][] = $instance->lang('emptyrecipientemail');
-			if($output['data']['generate']['email'] && !csfValidateEmail($generate['email'])) $output['errormessages'][] = $instance->lang('invalidrecipientemail');
+			if($output['data']['generate']['email'] && !csfmanager::csfValidateEmail($output['data']['generate']['email'])) $output['errormessages'][] = $instance->lang('invalidrecipientemail');
 			if(!$client_id) $output['errormessages'][] = $instance->lang('emptyclient');
 			if($client_id && !isset($output['data']['clients'][$client_id])) $output['errormessages'][] = $instance->lang('invalidclient');
 			if(!intval($output['data']['generate']['server'])) $output['errormessages'][] = $instance->lang('emptyserver');
